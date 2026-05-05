79bb411bd30362a7825eb694f759a0e2cf2bf7eb
Francesco <893349@stud.unive.it>
2026-05-05 16:04:22 +0200

    improvments

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
-    pygame.init()
+    _ = pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    _ = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+
+        _ = screen.fill("black")
+
+        updatable.update(dt)
+        for i in drawable:
+            i.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
